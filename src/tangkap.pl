e862f397f950501db6a0da41644744c1d09b6c66
Radhityan <satyaradhit1@gmail.com>
2026-05-19 20:21:20 +0700

    feat: tangkap

---
+++ src/tangkap.pl
@@ -0,0 +1,14 @@
+:-dynamic (uni\1).
+
+tangkap(Target):-
+    uni(Target),
+    tangan(Target, Tangan),
+    bagiKartu(Target, 2, Tangan),
+    retract(uni(Target)).
+
+tangkap(Target):-
+    \+uni(Target),
+    giliran(Pemain),
+    tangan(Pemain, Tangan),
+    bagiKartu(Pemain, 2, Tangan).
+
