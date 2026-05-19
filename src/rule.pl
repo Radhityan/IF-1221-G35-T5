5b22937cea2ab301a4db5ac33f88cc78706cd3e6
Fikri <fikrihariyawan@gmail.com>
2026-05-19 11:33:00 +0700

    feat: add pemain

--- src/rule.pl
+++ src/rule.pl
@@ -1,24 +1,117 @@
+consult('utilitasList.pl').
+consult('aksi.pl').
+consult('fakta.pl').
+:- dynamic(giliran/1).
+:- dynamic(urutan/1). % buat sementara
+:- dynamic(pemain/1). 
+:- dynamic(tangan/2).
+:- dynamic(discardPile/1).
 
-/* RULES */
+panjang([], 0).
+panjang([H|T], Hasil) :-
+    panjang(T, HasilLama),
+    Hasil is HasilLama + 1.
+
+cariKartu(Hasil) :-
+    retractall(list_kartu(_)),
+    assertz(list_kartu([])),
+    kartu(Warna, Jenis),
+    update_list(kartu(Warna, Jenis)),
+    fail.
+
+cariKartu(Hasil) :- list_kartu(Hasil), !.
+
+update_list(Kartu) :-
+    retract(list_kartu(List)),
+    ListBaru = [Kartu|List],
+    assertz(list_kartu(ListBaru)).
+
 startGame :-
+    retractall(giliran(_)),
+    retractall(giliran1(_)), 
+    retractall(urutan(_)),
+    retractall(tangan(_)),
+    retractall(discardPile(_)),
     write('Masukkan jumlah pemain: '),
     read(Jumlah),
-    cek(Jumlah).
+    cek(Jumlah),
+    acakUrutan,
+    discardTop(KartuAwal).
+
 cek(Jumlah):-
     Jumlah >= 2, Jumlah =< 4, !,
-    inputPemain(1, Jumlah, [], ListPemain),
+    inputPemain(1, Jumlah),
     nl.
+
 cek(_):-
     write('Mohon masukkan angka antara 2-4'), nl,
     startGame.
 
-inputPemain(Index, Max, List, Atribut) :-
-    Index > Max, !,
-    reverse(List, Atribut).
+inputPemain(Index, Max) :-
+    Index > Max, !.
 
-inputPemain(Index, Max, List, Atribut) :-
+inputPemain(Index, Max) :-
     Index =< Max,
     write('Masukkan nama pemain '), write(Index), write(': '),
-    read(Nama), validasi(Nama, List, Valid),
+    read(Nama), validasi(Nama, Valid),
+    assertz(pemain(Valid)), 
     Index2 is Index + 1,
-    inputPemain(Index2, Max, [Valid | List], Atribut).
+    inputPemain(Index2, Max).
+
+validasi(Nama, Valid) :-
+    urutan(Nama), !, 
+    write('Nama sudah digunakan. Masukkan nama lain: '),
+    read(Nama1), validasi(Nama1, Valid).
+
+validasi(Nama, Nama).
+
+acakUrutan :-
+    semuaPemain(ListPemain),
+    acakList(ListPemain, ListUrutan),
+    assertz(urutan(ListUrutan)), 
+    urutanPemain(ListUrutan),
+    pembagianKartu(ListUrutan).
+
+semuaPemain(_) :-
+    pemain(Nama),
+    assertz(giliran1(Nama)),
+    fail.
+
+semuaPemain(ListAkhir) :-
+    hasil(ListAkhir).
+
+hasil([Nama | Sisa]) :-
+    retract(giliran1(Nama)), !,
+    hasil(Sisa).
+hasil([]).
+
+acakList([], []) :- !.
+acakList(ListAwal, [Pilihan | Sisa]) :-
+    panjang(ListAwal, PanjangList),
+    random(0, PanjangList, Acak),
+    hapusElemenList(Acak, ListAwal, SisaElmt),
+    acakList(SisaElmt,Sisa).
+
+hapusElemenList(1, [_|T], T).
+hapusElemenList(N, [H|T], [H|HasilTail]) :-
+    N > 1,
+    N1 is N - 1,
+    write(HasilTail),
+    hapusElemenList(N1, T, HasilTail).
+
+urutanPemain([Pertama | Sisa]) :-
+    write('Urutan pemain: '), write(Pertama),
+    sisaPemain(Sisa), nl.
+sisaPemain([Kedua|Sisa]) :-
+    write(' - '), write(Kedua),
+    sisaPemain(Sisa).
+
+pembagianKartu(ListUrutan) :-
+    write('Setiap pemain mendapatkan 7 kartu acak.'), nl,
+    random(1, 54, deckKartu),
+    pembagianKartu(ListUrutan).
+
+discardTop(KartuAwal) :-
+    panjang(deckKartu, PanjangList),
+    random(0,PanjangList, KartuAwal),
+    write('Kartu discard top: '), write(KartuAwal).
